52d98d3e904f67c116ecd877feeeacdc3e4d181d
¨jeferson-davila¨ <davilajeferson468@gmail.com>
2022-11-09 23:19:34 -0600

    Subclase Doctor

--- src/tareagrupalmoduloseis/Main.java
+++ src/tareagrupalmoduloseis/Main.java
@@ -15,6 +15,27 @@ public class Main {
         System.out.println(persona.getDireccion());
         System.out.println(persona.getEdad());
         System.out.println(persona.getTelefono());
+        
+        System.out.println("");
+        Doctor doctor = new Doctor();
+        doctor.setNombres("Mario Ruben");
+        doctor.setApellidos("Estrada Zuniga");
+        doctor.setDireccion("Barrio Terminal");
+        doctor.setTelefono("504 34213522");
+        doctor.setEdad(40);
+        doctor.setEspecializacion("Cirujano");
+        doctor.setLugarTrabajo("Hospital General");
+        doctor.setAnioExperiencia(5);
+        
+        System.out.println(doctor.getNombres());
+        System.out.println(doctor.getApellidos());
+        System.out.println(doctor.getDireccion());
+        System.out.println(doctor.getTelefono());
+        System.out.println(doctor.getEdad());
+        System.out.println(doctor.getEspecializacion());
+        System.out.println(doctor.getLugarTrabajo());
+        System.out.println(doctor.getAnioExperiencia());
+        
     }
 
 }
